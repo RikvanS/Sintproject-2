e6c4f889272b1f9f6ce8e265100a96e153d02bf0
Rik van Straaten <rikvstraaten@gmail.com>
2018-11-22 15:55:13 +0100

    minor layout and style fixes

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('list', 'ListController@index');
+Route::get('list', 'ListController@index')->name('list');
 Route::post('list', 'ListController@create');
 Route::post('delete', 'ListController@delete');
 Route::post('update', 'ListController@update');
