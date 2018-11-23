45bdf58331d610b1c6f86712cb10543de902e880
Rik van Straaten <rikvstraaten@gmail.com>
2018-11-23 15:39:32 +0100

    Main page less empty

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,19 @@
 @extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Welkom!</div>
+
+                <div class="card-body">
+                    Wat is Sinterklaas nou zonder verlanglijstje? Hier kun je je lijstje maken en aanpassen.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa suscipit fuga doloribus numquam sit sapiente quibusdam nisi minima. Repudiandae ducimus nostrum nobis magnam voluptatem corporis facere tempore dolorum nisi quibusdam!
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
